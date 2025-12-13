eb98449d935c101ed6bb5c0a972ada7260056218
murad403 <mdm78774@gmail.com>
2025-12-13 10:01:32 +0600

    api integrate admin header

--- app/componensts/shared/AdminHeader.tsx
+++ app/componensts/shared/AdminHeader.tsx
@@ -10,7 +10,7 @@ type TProps = {
 
 const AdminHeader = ({title, description}: TProps) => {
   const {data, isLoading} = useProfileQuery(undefined);
-  // console.log(data);
+  // console.log(data?.profile?.profile_picture);
   return (
     <div className='bg-common p-5 rounded-xl flex justify-between items-center border border-border-color-color'>
       <div>
@@ -18,10 +18,10 @@ const AdminHeader = ({title, description}: TProps) => {
         <p className='text-[16px] font-medium text-title'>{description}</p>
       </div>
       <div className="flex items-center gap-5">
-        <Image src={admin} alt="admin image" width={60} height={60} className="rounded-full"></Image>
+        <Image src={data?.profile?.profile_picture || admin} alt="admin image" width={60} height={60} className="rounded-full"></Image>
         <div>
-            <h3 className='font-semibold text-xl text-header'>Rahik Ahsan</h3>
-            <p className='font-medium text-title text-[16px]'>Admin</p>
+            <h3 className='font-semibold text-xl text-header capitalize'>Richel</h3>
+            <p className='font-medium text-title text-[16px] capitalize'>{data?.name}</p>
         </div>
       </div>
     </div>
