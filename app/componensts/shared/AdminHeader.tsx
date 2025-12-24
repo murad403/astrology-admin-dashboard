d841a83d9f517fd6c112e05da67a6537c0e89b8a
murad403 <mdm78774@gmail.com>
2025-12-24 16:25:16 +0600

    add link in admin header

--- app/componensts/shared/AdminHeader.tsx
+++ app/componensts/shared/AdminHeader.tsx
@@ -2,14 +2,15 @@
 import Image from "next/image";
 import admin from "@/public/admin.png";
 import { useProfileQuery } from "@/redux/features/auth/authApi";
+import Link from "next/link";
 
 type TProps = {
   title: string;
   description: string;
 }
 
-const AdminHeader = ({title, description}: TProps) => {
-  const {data} = useProfileQuery(undefined);
+const AdminHeader = ({ title, description }: TProps) => {
+  const { data } = useProfileQuery(undefined);
   // console.log("profile data", data);
   return (
     <div className='bg-common p-5 rounded-xl flex justify-between items-center border border-border-color-color'>
@@ -18,10 +19,12 @@ const AdminHeader = ({title, description}: TProps) => {
         <p className='text-[16px] font-medium text-title'>{description}</p>
       </div>
       <div className="flex items-center gap-5">
-        <Image src={data?.profile?.profile_picture_url || admin} alt="admin image" width={500} height={500} className="rounded-full size-[60px]"></Image>
+        <Link href={"/settings/personal-information"}>
+          <Image src={data?.profile?.profile_picture_url || admin} alt="admin image" width={500} height={500} className="rounded-full size-[60px]"></Image>
+        </Link>
         <div>
-            <h3 className='font-semibold text-xl text-header capitalize'>{data?.name}</h3>
-            <p className='font-medium text-title text-[16px] capitalize'>admin</p>
+          <h3 className='font-semibold text-xl text-header capitalize'>{data?.name}</h3>
+          <p className='font-medium text-title text-[16px] capitalize'>admin</p>
         </div>
       </div>
     </div>
